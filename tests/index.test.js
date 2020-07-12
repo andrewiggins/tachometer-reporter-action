56ba5a9de1cfd618584fa0986e49df9f2d2a227e
Andre Wiggins <andrewiggins@live.com>
2020-07-11 19:32:08 -0700

    Rename versions to frameworks
    to differentiate between option names and values

--- tests/index.test.js
+++ tests/index.test.js
@@ -7,8 +7,8 @@ const { buildReport } = require("../lib/index");
 const testResultsPath = path.join(__dirname, "test-results.json");
 const testResults = JSON.parse(readFileSync(testResultsPath, "utf8"));
 
-const localVersion = "local-version";
-const baseVersion = "base-version";
+const localVersion = "local-framework";
+const baseVersion = "base-framework";
 
 test("Initial test", () => {
 	const report = buildReport(testResults, localVersion, baseVersion);
