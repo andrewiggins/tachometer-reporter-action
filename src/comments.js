9c2ca2b9328e6d67f83c9c39d20735775c238645
Andre Wiggins <andrewiggins@live.com>
2020-07-18 10:36:11 -0700

    Add read/writeComment functions

--- src/comments.js
+++ src/comments.js
@@ -1,4 +1,26 @@
-function acquireCommentLock() {
+/** @type {(c: import('./global').CommentData) => boolean} */
+const commentMatcher = (c) =>
+	c.user.type === "Bot" &&
+	/<sub>[\s\n]*tachometer-reporter-action/.test(c.body);
+
+const footer = `\n\n<a href="https://github.com/andrewiggins/tachometer-reporter-action"><sub>tachometer-reporter-action</sub></a>`;
+
+async function initiateCommentLock() {
+	// TODO: Consider adding an initiateCommentLock function which finds all
+	// workflow runs associated with this PR and searches for runs whose jobs
+	// contain this action. The job whose run_id and job/step index is first
+	// creates a new comment. All others wait for comment to be created.
+	//
+	// Creating a comment per workflow def should simplify this a bit since you'll
+	// only have to search the jobs for the current workflow instead of trying to
+	// find all workflow runs. Would mean the footer and comment matcher needs to
+	// be customized per workflow with the workflow name.
+	//
+	// Don't worry about steps since steps must run sequentially. Multiple
+	// reporter actions in one job can't parallelly try to initiate comment lock.
+}
+
+async function acquireCommentLock() {
 	// config values:
 	// - minLockHold: Minimum amount of time lock must be consistently held before
 	//   safely assuming it was successfully acquired. Default: 500ms (or 1s?)
@@ -26,32 +48,26 @@ function acquireCommentLock() {
 }
 
 /**
- * Create a PR comment, or update one if it already exists
- * @param {import('./global').GitHubActionClient} github,
- * @param {import('./global').GitHubActionContext} context
- * @param {(comment: import('./global').CommentData | null) => string} getCommentBody
+ * Read a comment matching with matching regex
+ * @typedef {{ owner: string; repo: string; issue_number: number; }} CommentInfo
+ * @param {import('./global').GitHubActionClient} github
+ * @param {CommentInfo} commentInfo
+ * @param {(c: import('./global').CommentData) => boolean} matches
  * @param {import('./global').Logger} logger
  */
-async function postOrUpdateComment(github, context, getCommentBody, logger) {
-	const footer = `\n\n<a href="https://github.com/andrewiggins/tachometer-reporter-action"><sub>tachometer-reporter-action</sub></a>`;
-	const commentInfo = {
-		...context.repo,
-		issue_number: context.issue.number,
-	};
-
-	logger.startGroup(`Updating PR comment`);
-
+async function readComment(github, commentInfo, matches, logger) {
 	/** @type {import('./global').CommentData} */
 	let comment;
+
 	try {
-		logger.info(`Looking for existing comment...`);
+		logger.info(`Trying to read matching comment...`);
+
+		// Assuming comment is in the first page of results for now...
+		// https://docs.github.com/en/rest/reference/issues#list-issue-comments
 		const comments = (await github.issues.listComments(commentInfo)).data;
 		for (let i = comments.length; i--; ) {
 			const c = comments[i];
-			if (
-				c.user.type === "Bot" &&
-				/<sub>[\s\n]*tachometer-reporter-action/.test(c.body)
-			) {
+			if (matches(c)) {
 				comment = c;
 				logger.info(`Found comment! (id: ${c.id})`);
 				logger.debug(() => `Found comment: ${JSON.stringify(c, null, 2)}`);
@@ -59,22 +75,68 @@ async function postOrUpdateComment(github, context, getCommentBody, logger) {
 			}
 		}
 	} catch (e) {
-		logger.info("Error checking for previous comments: " + e.message);
+		logger.warn("Error trying to read comments: " + e.message);
 	}
+
+	return comment;
+}
+
+/**
+ * @param {import('./global').GitHubActionClient} github
+ * @param {CommentInfo} commentInfo
+ * @param {number} commentId
+ * @param {string} body
+ * @param {import('./global').Logger} logger
+ */
+async function writeComment(github, commentInfo, commentId, body, logger) {
+	logger.info(`Writing comment body (id: ${commentId})...`);
+	await github.issues.updateComment({
+		repo: commentInfo.repo,
+		owner: commentInfo.owner,
+		comment_id: commentId,
+		body,
+	});
+
+	logger.debug(() => `New body: ${body}`);
+}
+
+/**
+ * Create a PR comment, or update one if it already exists
+ * @param {import('./global').GitHubActionClient} github
+ * @param {import('./global').GitHubActionContext} context
+ * @param {(comment: import('./global').CommentData | null) => string} getCommentBody
+ * @param {import('./global').Logger} logger
+ */
+async function postOrUpdateComment(github, context, getCommentBody, logger) {
+	const commentInfo = {
+		...context.repo,
+		issue_number: context.issue.number,
+	};
+
+	// logger.startGroup(`Updating PR comment:`);
+	logger.info(`Updating PR comment:`);
+
+	// TODO: Need to get writer id to identify this run as having the lock
+
+	// TODO: How will getCommentBody handle if the comment already exists but with
+	// just the lock metadata? Perhaps acquireCommentLock returns if it created
+	// the comment? Perhaps initiateCommentLock takes an initial template?
+
+	// TODO: Replace with acquireCommentLock. Should acquireCommentLock return the
+	// comment once it is locked so we don't need to read it again?
+	let comment = await readComment(github, commentInfo, commentMatcher, logger);
 
 	if (comment) {
 		try {
-			logger.info(`Updating comment (id: ${comment.id})...`);
 			let updatedBody = getCommentBody(comment);
 			if (!updatedBody.includes(footer)) {
 				updatedBody = updatedBody + footer;
 			}
 
-			await github.issues.updateComment({
-				...context.repo,
-				comment_id: comment.id,
-				body: updatedBody,
-			});
+			// TODO: Write `removeCommentLock` function to remove the comment lock
+			// from updatedBody
+
+			await writeComment(github, commentInfo, comment.id, updatedBody, logger);
 		} catch (e) {
 			logger.info(`Error updating comment: ${e.message}`);
 			comment = null;
@@ -93,7 +155,7 @@ async function postOrUpdateComment(github, context, getCommentBody, logger) {
 		}
 	}
 
-	logger.endGroup();
+	// logger.endGroup();
 }
 
 module.exports = {
